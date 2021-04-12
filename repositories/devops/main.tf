5a4887dd3d706fc5950a4e885e68147b5a60f21e
Mayank Aggarwal <aggarwal.mayank@live.com>
2021-04-12 17:42:36 +0200

    repositories/devops-create_php_repo

---
+++ repositories/devops/main.tf
@@ -0,0 +1,21 @@
+locals {
+  teams_repository_memberships = {
+    bots = "admin"
+    all  = "push"
+  }
+  teams = data.terraform_remote_state.teams.outputs.teams
+}
+
+module "docker-base-php-fpm" {
+  source = "git@github.com:AppCodeOrg/terraform-modules.git//github/repository?ref=0.0.3"
+
+  teams = local.teams
+
+  name        = "docker-base-php-fpm"
+  description = "Base docker image for PHP"
+
+  enable_branch_protection = false
+
+  teams_repository_memberships = local.teams_repository_memberships
+
+}
