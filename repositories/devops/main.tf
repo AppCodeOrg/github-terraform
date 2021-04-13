499feeb7f87d7125b057c4b63e405cf6ca6e01a0
Mayank Aggarwal <aggarwal.mayank@live.com>
2021-04-13 08:17:48 +0200

    repositories/devops-nginx_repo

--- repositories/devops/main.tf
+++ repositories/devops/main.tf
@@ -19,3 +19,17 @@ module "docker-base-php-fpm" {
   teams_repository_memberships = local.teams_repository_memberships
 
 }
+
+module "docker-base-nginx" {
+  source = "git@github.com:AppCodeOrg/terraform-modules.git//github/repository?ref=0.0.3"
+
+  teams = local.teams
+
+  name        = "docker-base-nginx"
+  description = "Base docker image for Nginx"
+
+  enable_branch_protection = false
+
+  teams_repository_memberships = local.teams_repository_memberships
+
+}
