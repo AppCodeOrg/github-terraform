6893873c02607bed9deb381eca7846804aee624d
aggarwal-mayank <aggarwal.mayank@live.com>
2021-05-02 17:42:47 +0200

    users-ma_appcode_add

--- users/main.tf
+++ users/main.tf
@@ -2,3 +2,17 @@ locals {
   teams = data.terraform_remote_state.teams.outputs.teams
 }
 
+resource "github_membership" "mayank-aggarwal-org-member" {
+  username = "mayank-aggarwal-appcode"
+  role     = "admin"
+}
+
+resource "github_team_membership" "mayank-aggarwal-team-member" {
+  team_id  = lookup(local.teams, each.key)
+  role     = each.value
+
+  username = github_membership.mayank-aggarwal-org-member.username
+  for_each = {
+    devops = "member"
+  }
+}
